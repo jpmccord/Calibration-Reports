c3bea08e657968a49ef900e861c1ea95e7f6297b
James McCord <mccord.james@epa.gov>
2018-10-15 10:03:15 -0400

    Function to combine multi-batch runs

--- readThermoTargetedReport.R
+++ readThermoTargetedReport.R
@@ -9,14 +9,41 @@ check.packages <- function(package){
 check.packages(c("tidyverse", "readxl"))
 
 readThermoTargetedSheet <- function(sheetname, file) {
+  
+checkLong <- length(read_excel(file,
+                        sheet = sheetname,
+                        skip = 4,
+                        trim_ws = TRUE,
+                        n_max = 3)) > 25
 
-the.sheet <- as.tibble(
+if (checkLong) {
+  
+  the.sheet <- as.tibble(
       read_excel(file,
                  sheet = sheetname,
                  skip = 4,
                  trim_ws = TRUE,
-                 col_types = "text",
+                 col_types = c("text", "text", "text", "text", "numeric","numeric",
+                               "text","text","text","numeric","text","numeric",
+                               "text","text","numeric","numeric","numeric","numeric",
+                               "numeric","numeric","numeric","numeric","numeric","numeric",
+                               "numeric","numeric","text","numeric","numeric","numeric",
+                               "date","numeric","text","text","text","numeric",
+                               "numeric","numeric","numeric","numeric","numeric","numeric",
+                               "text","text","text","text","text"),
                  na = c("","NA","NF","ND"))  )
+
+} else {
+  the.sheet <- as.tibble(
+    read_excel(file,
+               sheet = sheetname,
+               skip = 4,
+               trim_ws = TRUE,
+               col_types = c("text","text","text","text","numeric","numeric",
+                             "numeric","numeric","numeric","numeric","numeric","text",
+                             "text","text","numeric","text"),
+               na = c("","NA","NF","ND"))  )
+}
 
 result <- head(the.sheet,-6)
 
@@ -39,10 +66,26 @@ summary <- bind_rows(vector) %>%
   mutate_at(vars(one_of("Exp Amt","Calc Amt","%Diff", "%RSD-AMT","Response Ratio","Area","Height","ISTD Area","ISTD Ht",
                         "RT","Rel RT", "Del RT", "S/N","Start Time","End Time","Start Height","End Height","Search Window",
                         "Duration","Sample Wt", "Sample Vol", "ISTD Base Amt","ISTD Calc Amt", "Dilution Factor")),
-            funs(as.numeric))
+            funs(as.numeric)) %>%
+  mutate(batch = basename(file))
 
 }
 
-#file <- c("C:/Users/Orbitrap-Processing/Documents/R Scripts/Calibration-Reports/Sample Long Report.xls")
+readBatchedThermoExperiment <- function(filelist) {
+  
+batch <- sapply(filelist, FUN = readThermoTargetedReport)
+
+bind_rows(batch) #does not work with mixed short and long report formats, extend functionality later
+  
+}
+
+#file <- c("Sample Long Report.xls")
+#shortfile <- c("Sample Short Report.xls")
+
+#filist <- c(file,shortfile)
 
 #summary <- readThermoTargetedReport(file)
+
+#readBatchedThermoExperiment(filist)
+
+
