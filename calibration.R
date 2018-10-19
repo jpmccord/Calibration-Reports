06e2c2a3604d9ab301e932699a162a5f33ca76fe
jpmccord <33838029+jpmccord@users.noreply.github.com>
2018-10-19 10:18:10 -0400

    Fixed issues caused by previous commit

--- calibration.R
+++ calibration.R
@@ -5,7 +5,7 @@ check.packages <- function(package){
   sapply(package, require, character.only = TRUE)
 }
 
-check.packages(c("tidyverse", "readxl", "investr"))
+check.packages(c("tidyverse", "readxl", "investr", "broom"))
 
 inputFile <- "Sample Short Report.XLS"
 
@@ -35,9 +35,9 @@ invest(calCurve.model.lm1x, y0 = 0.5, interval = "Wald")
 invest(calCurve.model.lm1x, y0 = 0.5, interval = "percentile", nsim = 9999)
 
 
-calibrateCmp <- function(cmp, dataset) {
+calibrateCmp <- function(Comp, dataset) {
   
-  calCurve <- dplyr::filter(data, Sample.Type == "Standard", is.na(Peak.Status) | Peak.Status != "Excluded", Cmp == cmp)
+  calCurve <- dplyr::filter(data, Sample.Type == "Standard", is.na(Peak.Status) | Peak.Status != "Excluded", Cmp == Comp)
   
   formula.lm <- formula(Area.Ratio ~ Exp.Amt)
   formula.qd <- formula(Area.Ratio ~ Exp.Amt + I(Exp.Amt^2)) 
@@ -58,14 +58,14 @@ calibrateCmp <- function(cmp, dataset) {
                              weights = 1/Exp.Amt^2,
                              data = as.data.frame(calCurve))  
   model.labels = c("Linear weight = 1/x",
-            "Linear weight = 1/x^2",
-            "Quadratic weight  = 1/x",
-            "Qudratic weight = 1/x^2")
+                   "Linear weight = 1/x^2",
+                   "Quadratic weight  = 1/x",
+                   "Qudratic weight = 1/x^2")
   
   model.list <- list(calCurve.model.lm1x, calCurve.model.lm1x2, calCurve.model.qd1x,calCurve.model.qd1x2)
   
   names(model.list) <- model.labels
-    
+  
   model_frame <- enframe(model.list, name = "modelName", value = "model")
   
   fits <- map(.x = model_frame$model, .f = broom::glance) %>%
@@ -73,7 +73,7 @@ calibrateCmp <- function(cmp, dataset) {
   
   which.model = which(fits$adj.r.squared == max(fits$adj.r.squared))
   
-  return = list(model.labels[[which.model]], model.list[[which.model]])
+  return = list(model.list[[which.model]])
   
   return(return)
 }
@@ -82,78 +82,96 @@ calibrateDataset <- function(dataset) {
   quant_compounds <- dataset %>% group_by(Cmp) %>%
     summarize(ISTD.Response = mean(ISTD.Response)) %>%
     filter(!is.na(ISTD.Response))
-
-sapply(quant_compounds$Cmp, dataset = data, FUN = calibrateCmp)
+  
+  sapply(quant_compounds$Cmp, dataset = data, FUN = calibrateCmp)
 }
 
 getSamplePredictions <- function(dataset){
+  
+  dataset_models <- calibrateDataset(dataset)
+  
+  sample_measures <- dplyr::filter(dataset, Sample.Type == "Sample") %>%
+    group_by(Cmp) %>%
+    select(Cmp,Sample.Name,Sample.ID,Raw.Response,ISTD.Response) %>%
+    mutate(Raw.Response = ifelse(is.na(Raw.Response), 0, Raw.Response)) %>%
+    mutate(Area.Ratio = Raw.Response/ISTD.Response) %>%
+    filter(Cmp %in% names(dataset_models))
+  
+  reverse.calibration <- function(subject, cmp, dataset) {
+    measures <- dplyr::filter(dataset, Sample.ID == subject, Cmp == cmp)$Area.Ratio
+    
+    calCurve.model <- dataset_models[[cmp]]
+    
+    calCurve <- as.data.frame(calCurve.model$model)# extract original calCurve from model for inverse estimates
 
-dataset_models <- calibrateDataset(dataset)
-
-sample_measures <- dplyr::filter(dataset, Sample.Type == "Sample") %>%
-  group_by(Cmp) %>%
-  select(Cmp,Sample.Name,Sample.ID,Raw.Response,ISTD.Response) %>%
-  mutate(Area.Ratio = Raw.Response/ISTD.Response) %>%
-  filter(Cmp %in% names(dataset_models))
-
-reverse.calibration <- function(subject, Cmp, dataset) {
-  measures <- dplyr::filter(dataset, Sample.ID == subject, Cmp == cmp)$Area.Ratio
-  
-  lower.bound = min(calCurve$Exp.Amt) - 0.5*(max(calCurve$Exp.Amt)-min(calCurve$Exp.Amt))
-  upper.bound = 2*max(calCurve$Exp.Amt) + (max(calCurve$Exp.Amt)-min(calCurve$Exp.Amt))
-  
-  calCurve.model <- dataset_models[[Cmp]]
-  
-  results <- try(invest(calCurve.model,
-                        y0 = measures,
-                        interval = "Wald",
-                        extendInt = "yes",
-                        lower = lower.bound,
-                        upper = upper.bound,
-                        maxiter = 1e7),
-                 silent = TRUE)
-  
-  if (inherits(results, "try-error")) {
-    #print(paste("Prediction bounds not contained in the search interval (", lower.bound, 
-    #           ", ", upper.bound, "). ", 
-    #           "Try tweaking the values of lower and upper.", sep = ""))
+    lower.bound = min(calCurve$Exp.Amt) - 0.5*(max(calCurve$Exp.Amt)-min(calCurve$Exp.Amt))
+    upper.bound = 2*max(calCurve$Exp.Amt) + (max(calCurve$Exp.Amt)-min(calCurve$Exp.Amt))
     
-    results.frame <- data.frame(subject = as.character(subject),
-                                estimate = as.numeric(NA),
-                                lower = as.numeric(NA),
-                                upper = as.numeric(NA))
     
-    return(results.frame)
     
-  } else {
     
-    results.frame <- tibble(subject = as.character(subject),
-                                estimate = results$estimate,
-                                lower = results$lower,
-                                upper = results$upper,
-                                Cmp = Cmp)
+    results <- try(invest(calCurve.model,
+                          y0 = measures,
+                          interval = "Wald",
+                          extendInt = "yes",
+                          lower = lower.bound,
+                          upper = upper.bound,
+                          maxiter = 1e7),
+                   silent = TRUE)
     
-    return(results.frame)
+    if (inherits(results, "try-error")) {
+      #print(paste("Prediction bounds not contained in the search interval (", lower.bound, 
+      #           ", ", upper.bound, "). ", 
+      #           "Try tweaking the values of lower and upper.", sep = ""))
+      
+      results.frame <- data.frame(subject = as.character(subject),
+                                  estimate = as.numeric(NA),
+                                  lower = as.numeric(NA),
+                                  upper = as.numeric(NA))
+      
+      return(results.frame)
+      
+    } else {
+      
+      results.frame <- tibble(subject = as.character(subject),
+                              estimate = results$estimate,
+                              lower = results$lower,
+                              upper = results$upper,
+                              Cmp = cmp)
+      
+      return(results.frame)
+    }
   }
+  
+  subjects <- unique(sample_measures$Sample.ID)
+  
+  Cmps <- unique(sample_measures$Cmp)
+  
+  predictsubjects<- function(Compound) {lapply(subjects, cmp = Compound, FUN = reverse.calibration, dataset = sample_measures) %>%
+      bind_rows()
+  }
+  
+  predictions <- lapply(Cmps, FUN=predictsubjects) %>%
+    bind_rows()
+  
 }
 
-subjects <- unique(sample_measures$Sample.ID)
+calmodels <- calibrateDataset(data)
 
-Cmps <- unique(sample_measures$Cmp)
+calpredictions <- getSamplePredictions(data)
 
-predictsubjects<- function(Cmp) {lapply(subjects, Cmp = Cmp, FUN = reverse.calibration, dataset = sample_measures) %>%
-  bind_rows()
-}
+plot_file <- calpredictions %>%
+  mutate(Cmp = as.factor(Cmp)) %>%
+  mutate(color_me = ifelse(subject == "527", TRUE, FALSE)) %>%
+  filter(Cmp %in% c("PFOA", "PFOS", "GenX"))
 
-predictions <- lapply(Cmps, FUN=predictsubjects) %>%
-  bind_rows()
-
-}
-
-ggplot(filter(predictions, Cmp == "PFOA")) +
+ggplot() +
   theme_minimal()+
-  geom_point(aes(y = Cmp, x = estimate), position = position_jitter(height = 0.05)) +
-  geom_tile(aes(y = Cmp, x = predictions[[1,2]], width = (upper-lower)/2, height = 0.05), alpha = 0.05)
+  geom_point(data = filter(plot_file, color_me == FALSE), aes(y = Cmp, x = estimate), position = position_jitter(height = 0.05), shape = 5, color = "blue", alpha = 0.25) +
+  geom_point(data = filter(plot_file, color_me == TRUE), aes(y = Cmp, x = estimate), color = "green", shape = 21) +
+  geom_tile(data = filter(plot_file, color_me == TRUE), aes(x = estimate, y = Cmp, width = (lower-upper)/2), height = 0.5, alpha = 0.1)
+
+  
 
 
   
